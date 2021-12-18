3bbacb49361af1eff6cf6b0049b948dcbda37c12
samnj <sam.nj7@gmail.com>
2021-12-18 01:30:00 -0300

    exs up to 3.6

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const found = persons.find(p => p.id === Number(req.params.id))
+  found ? res.json(found) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(p => p.id !== Number(req.params.id))
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const duplicated = persons.find(p => p.id === Number(req.params.id))
+
+  if (!body.name) {
+    return res.status(400).json({ error: 'name missing' })
+  } else if (!body.number) {
+    return res.status(400).json({ error: 'number missing' })
+  } else if (duplicated) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000) + 1,
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+
+  res.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
