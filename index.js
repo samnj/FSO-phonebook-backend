d106611fa2587a7b8619b16cd9d72ec550cafcf5
samnj <sam.nj7@gmail.com>
2021-12-26 14:28:05 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const Person = require('./models/phonebook')
 
 app.use(express.json())
-
 app.use(express.static('build'))
 
 morgan.token('postedData', (req) => {
@@ -12,72 +13,47 @@ morgan.token('postedData', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postedData'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/people', (req, res) => {
+  Person.find({}).then(p => {
+    res.json(p)
+  })
 })
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}</p>`)
+  //TO-DO
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const found = persons.find(p => p.id === Number(req.params.id))
-  found ? res.json(found) : res.status(404).end()
+app.get('/api/people/:id', (req, res) => {
+  Person.findById(req.params.id).then(p => {
+    res.json(p)
+  })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  persons = persons.filter(p => p.id !== Number(req.params.id))
+app.delete('/api/people/:id', (req, res) => {
+  people = people.filter(p => p.id !== Number(req.params.id))
   res.status(204).end()
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/people', (req, res) => {
   const body = req.body
-  const duplicated = persons.find(p => p.id === Number(req.params.id))
 
   if (!body.name) {
     return res.status(400).json({ error: 'name missing' })
   } else if (!body.number) {
     return res.status(400).json({ error: 'number missing' })
-  } else if (duplicated) {
-    return res.status(400).json({ error: 'name must be unique' })
   }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 10000) + 1,
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(newPerson)
-
-  res.json(newPerson)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://sam:${password}@cluster0.pfdpo.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  person.save().then(({ name, number }) => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
